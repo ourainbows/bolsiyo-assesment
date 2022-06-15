6f802926d829793f98887f36620ba11857ae213a
ourainbows <casasjuanfelipe@gmail.com>
2022-06-15 00:52:08 -0500

    Generated search-bar component and render it on home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ]
 })
 export class HomeModule { }
