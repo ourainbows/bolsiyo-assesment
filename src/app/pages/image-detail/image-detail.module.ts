6e5aa3bfef453c59d4b1538d6a3971d4bc3f097b
ourainbows <casasjuanfelipe@gmail.com>
2022-06-15 00:32:54 -0500

    Created folder structure

---
+++ src/app/pages/image-detail/image-detail.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImageDetailRoutingModule } from './image-detail-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ImageDetailRoutingModule
+  ]
+})
+export class ImageDetailModule { }
---
+++ src/app/pages/images/images.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ImagesRoutingModule } from './images-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ImagesRoutingModule
+  ]
+})
+export class ImagesModule { }
