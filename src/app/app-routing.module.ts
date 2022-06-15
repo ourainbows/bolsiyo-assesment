a37d6a6fef89be9692e23e5e224ced4f314321da
ourainbows <casasjuanfelipe@gmail.com>
2022-06-15 00:38:29 -0500

    Added basic routing in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./pages/images/images.module').then(m => m.ImagesModule) },
+  { path: 'image-detail', loadChildren: () => import('./pages/image-detail/image-detail.module').then(m => m.ImageDetailModule) },
+  { path: '**', redirectTo: '' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
