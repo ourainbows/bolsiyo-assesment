9617552f4dcde8ce5dd16fdc28108a52d0878ce2
ourainbows <casasjuanfelipe@gmail.com>
2022-06-17 00:55:53 -0500

    Generated footer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,13 @@ import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { ImagesGridComponent } from './components/images-grid/images-grid.component';
 import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [SearchBarComponent, ImagesGridComponent, NavComponent],
+  declarations: [SearchBarComponent, ImagesGridComponent, NavComponent, FooterComponent],
   imports: [CommonModule, FormsModule, RouterModule],
-  exports: [SearchBarComponent, ImagesGridComponent, NavComponent],
+  exports: [SearchBarComponent, ImagesGridComponent, NavComponent, FooterComponent],
 })
 export class SharedModule {}
