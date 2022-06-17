02eb3e03b2c0d866dc94a29df0436488a918908d
ourainbows <casasjuanfelipe@gmail.com>
2022-06-16 23:27:06 -0500

    Add info to view what are you searching on image component

--- src/app/pages/images/images.component.ts
+++ src/app/pages/images/images.component.ts
@@ -12,10 +12,11 @@ export class ImagesComponent implements OnInit {
   constructor(private router: ActivatedRoute, private imagesService: ImagesService) { }
   
   public images: imageModel[] = [];
+  public query : string = '';
 
   ngOnInit(): void {
     this.router.queryParams.subscribe(params => {
-      console.log(params)
+      this.query = params['query'];
       this.imagesService.getImages(params['query'], params['category']).subscribe(images => {
         this.images = images.hits;
       });
