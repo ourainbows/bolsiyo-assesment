93c4f06c814de05d081b0bc90785d9587e96d1b5
ourainbows <casasjuanfelipe@gmail.com>
2022-06-16 21:58:25 -0500

    Hero and searchbar styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'hero': "url('https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
+      },
+      fontFamily: {
+        'inter': ['"Inter"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
